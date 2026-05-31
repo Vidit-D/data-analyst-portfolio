e54e1936a5cc75a082e21aba8394319b404fde9f
Vidit-D <viditdalmia@gmail.com>
2026-05-31 21:36:01 +0530

    Day 8

---
+++ 01-Python/Week2-Advanced-Python/notes/03-modules-and-packages/package/__init__.py
@@ -0,0 +1,3 @@
+''' This is a special file used in python to define packages and initialize namespaces.
+    You can keep this file emply but is necessary to have a file with this same name if you 
+    want to make your own packages and use them in your project '''
